c60efd7ca65baa11aa9ee00aaceb6e0ecbd3cb29
chenjm34827 <chenjm34827@hundsun.com>
2025-05-15 13:51:11 +0800

    build wheels

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pybind11
 ext_modules = [
     Extension(
         'my_ctools.core',
-        ['col_std_threadpool.cpp'],
+        ['col_std_threadpool.cpp'],  # 路径修复
         include_dirs=[pybind11.get_include()],
         language='c++',
         extra_compile_args=['-std=c++11', '-O3'],
@@ -12,9 +12,13 @@ ext_modules = [
 ]
 
 setup(
-    name='my_ctools',
+    name='my-ctools',
     version='0.1.0',
     author='KevinCJM',
+    description='Fast column-wise std calculator using C++ thread pool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
     packages=['my_ctools'],
     ext_modules=ext_modules,
     zip_safe=False,
